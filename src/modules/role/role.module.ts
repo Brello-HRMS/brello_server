59d2ed83ce98b83b41c60e827eb688ac62cdd48d
Mohd Samir <samirmohd701@gmail.com>
2026-05-21 19:09:05 +0530

    refactor: remove RoleController from RoleModule imports

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -4,12 +4,12 @@ import { GlobalSearchModule } from '../global-search/global-search.module';
 import { Role } from './entities/role.entity';
 import { RoleService } from './services/role.service';
 import { RoleRepository } from './repositories/role.repository';
+import { RoleController } from './controllers/role.controller';
 
 // RoleController is registered in RbacModule (which has all RBAC dependencies).
 // RoleModule only needs to export RoleService for other feature modules.
 @Module({
   imports: [TypeOrmModule.forFeature([Role]), GlobalSearchModule],
-  controllers: [RoleController],
   providers: [RoleService, RoleRepository],
   exports: [RoleService, RoleRepository],
 })
